562817e6ce4351d1a8fe441420eca56bad1fad36
Danny <danny@cultureiq.com>
2020-04-12 12:09:04 -0400

    websockets!

---
+++ backend/server.py
@@ -0,0 +1,47 @@
+import asyncio
+import websockets
+import json
+from enum import Enum
+
+
+class MessageType(Enum):
+	USER_JOINED = 'userJoined'
+
+
+current_users = set()
+
+async def notify_users(payload, users = None):
+	if users is None:
+		users = current_users
+
+	await asyncio.wait([user.send(payload) for user in users])
+
+
+async def register(websocket):
+	current_users.add(websocket)
+	message = json.dumps({'type': MessageType.USER_JOINED.value})
+	await notify_users(message)
+
+async def unregister(websocket):
+	current_users.remove(websocket)
+
+
+async def main(websocket, path):
+	print(path)
+	await register(websocket)
+	try:
+		async for message in websocket:
+			data = json.loads(message)
+			print(data, )
+			if data['type'] == MessageType.USER_JOINED.value:
+				name = data['name']
+				print(f'{name} just joined up!')
+				await notify_users({'type': MessageType.USER_JOINED.value, 'name': name})
+	finally:
+		await unregister(websocket)
+
+
+
+start_server = websockets.serve(main, 'localhost', 8000)
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
--- scrumsistant/src/app/dashboard/dashboard.component.ts
+++ scrumsistant/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
-
+import {webSocket, WebSocketSubject} from 'rxjs/webSocket'
+import {delay} from 'rxjs/operators'
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -8,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  socket: WebSocketSubject<any> | null = null
+  user = {name: "Punk"}
+  nameLocked = false
+  broadcastMsg = ''
+
   constructor() { }
 
   ngOnInit(): void {
+    this.socket = webSocket('ws://localhost:8000')
+    this.socket.subscribe(
+      serverMsg => this.handleIncoming(serverMsg),
+      err => console.log(err),
+      () => console.log('connection closed'),
+     )
+  }
+
+  lockName(): void{
+    this.nameLocked = true
+    this.socket.next({type: 'userJoined', name: this.user.name})
+  }
+
+  handleIncoming(msg){
+    console.log(msg)
+    if (msg.type == 'userJoined') {
+      if (msg.name != this.user.name){
+        this.broadcastMsg = `Say hello to ${msg.name}!`
+        console.log(this.broadcastMsg)
+      }
+    }
   }
 
 }
