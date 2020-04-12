62a7aad036de3869ed80da15321ba059677b3ec2
Danny <danny@cultureiq.com>
2020-04-11 23:39:49 -0400

    this is like 30 ideas mushed up

--- scrumsistant/src/app/pointing/pointing.component.ts
+++ scrumsistant/src/app/pointing/pointing.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {timer, interval} from 'rxjs';
+import {takeUntil} from 'rxjs/operators'
 
 @Component({
   selector: 'app-pointing',
@@ -7,9 +9,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PointingComponent implements OnInit {
 
+  // TODO: points should be configurable I guess?
+  points: number[] = [
+  1,
+  2,
+  3,
+  5,
+  8,
+  13,
+  20,
+]
+
+  votingStates = [
+    'preVoting',
+    'periVoting',
+    'postVoting',
+  ]
+  votingStateIndex = 0
+
+  allowedVotingTimeSeconds = 3
+
+  numVoters: number = 1 // TODO this will be more sophisticated
+  votes: number[]
+
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  selectPoint(pointValue: number){
+    this.votes.push(pointValue)
+    this.checkIfDone()
+  }
+
+  checkIfDone(){
+    if (
+      this.votes.length === this.numVoters &&
+      this.votingStateIndex === 1
+     ){
+      this.votingStateIndex = 2
+    }
+  }
+
+  beginVote(){
+    if (this.votingStates[this.votingStateIndex] !== 'preVoting') {
+      return
+    }
+    this.votingStateIndex = 1
+
+    let timer$ = timer((this.allowedVotingTimeSeconds + 1) * 1000)
+    let tickSource$ = interval(1000)
+    tickSource$.pipe(takeUntil(timer$)).subscribe(
+      tick => {
+        this.allowedVotingTimeSeconds = this.allowedVotingTimeSeconds - 1
+      },
+      () => {}, //onError
+      () => {
+        this.votingStateIndex = 2
+      },
+    )
   }
 
 }
