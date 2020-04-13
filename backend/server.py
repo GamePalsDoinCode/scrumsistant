bccaa59f43c629a1ccd716de859f338356c7b4ea
Mike Goodman <msgoodman90@gmail.com>
2020-04-13 01:49:12 -0400

    just trying to figure out how the hell python works

--- backend/server.py
+++ backend/server.py
@@ -4,28 +4,37 @@ import json
 
 from utils import register, unregister
 from structs import MessageType, get_info_dict
+
 WEBSOCKET_INFO_DICT = get_info_dict()
 from handler_funcs import (
-	handle_get_usernames,
-	handle_new_user_joined,
+    handle_get_usernames,
+    handle_new_user_joined,
 )
 
-async def main(websocket, path):
-	if not websocket in WEBSOCKET_INFO_DICT:
-		await register(websocket)
-	try:
-		print(WEBSOCKET_INFO_DICT)
-		async for message in websocket:
-			data = json.loads(message)
-			print(data)
-			if data['type'] == MessageType.USER_JOINED.value:
-				await handle_new_user_joined(websocket, data)
-			elif data['type'] == 'getUsernames':
-				await handle_get_usernames(websocket)
-	finally:
-		await unregister(websocket)
+class Server:
+    def __init__(self, info_dict):
+        self.info_dict = info_dict
+
+    async def setup(self, websocket, path):
+        if websocket not in self.info_dict:
+            await register(websocket)
+        try:
+            print(self.info_dict)
+            async for message in websocket:
+                data = json.loads(message)
+                print(data)
+                if data['type'] == MessageType.USER_JOINED.value:
+                    await handle_new_user_joined(websocket, data)
+                elif data['type'] == 'getUsernames':
+                    await handle_get_usernames(websocket)
+        finally:
+            await unregister(websocket)
+
+    def run(self):
+        start_server = websockets.serve(self.setup, 'localhost', 8000)
+        asyncio.get_event_loop().run_until_complete(start_server)
+        asyncio.get_event_loop().run_forever()
 
 
-start_server = websockets.serve(main, 'localhost', 8000)
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+s = Server(WEBSOCKET_INFO_DICT)
+s.run()
