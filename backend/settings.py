98884c9257f587feb8b14c1d33af4754ba97e12e
debuggingMyBrain <jacob.peschansky@gmail.com>
2020-04-19 19:32:23 -0400

    Response to code review

--- backend/settings.py
+++ backend/settings.py
@@ -9,9 +9,5 @@ def init_logging():
     if platform.system() == "Windows":
         # For us ghouls that are running on Windows, won't have a /var, so log locally
         log_filename = "scrum.log"
-    # I would imagine that in production that we would just have a long running service that would log over
-    # a long period, so we might have a size determined rotation, but during development I find it helpful
-    # to restart the logs every run, seeing as different runs are common
-    dev_handler = logging.handlers.RotatingFileHandler(log_filename, backupCount=10)
-    dev_handler.doRollover()
-    logging.basicConfig(level=logging.DEBUG, handlers=[dev_handler], format=logging_format)
+    
+    logging.basicConfig(level=logging.DEBUG, filename=log_filename, format=logging_format)
