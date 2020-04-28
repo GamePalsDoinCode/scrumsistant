06e359a439503a55d9d901664b6c30ac1a7cf5b8
Danny <danny@cultureiq.com>
2020-04-27 22:20:25 -0400

    fix our fixture

--- backend/tests/fixtures.py
+++ backend/tests/fixtures.py
@@ -1,23 +1,29 @@
-import websockets
 import pytest
+import websockets
+
 from ..server import Server
 
 WEBSOCKET_URI = "ws://localhost:8000"
+
+
 @pytest.fixture
 def server(event_loop):
-	server = Server({})
-	task = server.get_server_task(server.router)
-	event_loop.run_until_complete(task)
+    server_ = Server()
+    task = server_.get_server_task(server_.router)
+    event_loop.run_until_complete(task)
+
 
 @pytest.fixture
 async def client():
-	async with websockets.connect(WEBSOCKET_URI) as websocket:
-		yield websocket
+    async with websockets.connect(WEBSOCKET_URI) as websocket:
+        yield websocket
+
 
 @pytest.fixture
 async def send_message(server, client):
-	async def send_message(message):
-		await client.send(message)
-		greeting = await client.recv()
-		return greeting
-	return send_message
+    async def send_message(message):
+        await client.send(message)
+        greeting = await client.recv()
+        return greeting
+
+    return send_message
