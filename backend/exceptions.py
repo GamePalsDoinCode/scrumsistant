765249a6cfb75c6db97284221544df194eba24fb
Anthony Caggiano <onaiggaca@gmail.com>
2020-04-28 12:49:41 -0400

    adjusted custom errors based on feedback

--- backend/exceptions.py
+++ backend/exceptions.py
@@ -1,8 +1,6 @@
-class EntryNotFoundError(Exception):
+class RedisKeyNotFoundError(Exception):
     pass
 
-class UserInfoNotFoundError(Exception):
-    pass
 
 class UserNameTakenError(Exception):
     pass
--- backend/query_tools.py
+++ backend/query_tools.py
@@ -1,13 +1,14 @@
-from .structs import WebsocketInfo
 from .exceptions import *
+from .structs import WebsocketInfo
+
 
 def get_user_by_email(email, redis_client):
     user_pk = redis_client.get(email)
     if not user_pk:
-        raise EntryNotFoundError
+        raise RedisKeyNotFoundError('No user with that email')
     user_dict = redis_client.hgetall(f'user_{int(user_pk)}')
 
     if not user_dict:
-        raise UserInfoNotFoundError
+        raise RedisKeyNotFoundError('User info not found')
     new_user_obj = WebsocketInfo.deserialize(user_dict)
     return new_user_obj
