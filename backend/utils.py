f3b5e139b2f872ad2489eb7670b391910151b58f
Danny <danny@cultureiq.com>
2020-05-06 23:14:24 -0400

    add some tests to keep coverage up; finally nail hypothesis [SCRUM-47]

--- backend/utils.py
+++ backend/utils.py
@@ -2,12 +2,6 @@ from typing import Any, Dict
 
 from .scrum_types import REDIS_SAFE_DICT_INPUT
 
-# import asyncio
-# async def notify_users(payload, users=None):
-#     if users is None:
-#         users = WEBSOCKET_INFO_DICT.keys()
-#     await asyncio.wait([user.send(payload) for user in users])
-
 
 def cleanup_redis_dict(dict_from_redis: Dict[bytes, bytes]) -> Dict[str, str]:
     return {k.decode('utf8'): v.decode('utf8') for k, v in dict_from_redis.items()}
