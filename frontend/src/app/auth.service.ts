6d0927c979a6c3de16bc8aa2915a2b1da1307948
Danny <danny@cultureiq.com>
2020-05-30 21:29:01 -0400

    tidy up auth service

--- frontend/src/app/auth.service.ts
+++ frontend/src/app/auth.service.ts
@@ -1,18 +1,25 @@
 import {Injectable} from '@angular/core'
 import {HttpClient} from '@angular/common/http'
 import {Router, CanActivate, ActivatedRouteSnapshot} from '@angular/router'
-import {tap} from 'rxjs/operators'
+import {tap, switchMap} from 'rxjs/operators'
+import {of, Observable} from 'rxjs'
+import {WebsocketService} from './websocket.service'
 
 type valueof<T> = T[keyof T]
+interface LoginReturnType{
+  user: Scrum.User;
+  auth: string;
+}
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService implements CanActivate {
   errorMessage = ''
-  user: Scrum.User = {email: '', pk: null, displayName: '', is_PM: false}
+  user: Scrum.User
 
-  constructor(private http: HttpClient, private router: Router) {}
+  constructor(private http: HttpClient, private router: Router, private websocket: WebsocketService) {}
 
   isAuthenticated() {
     // TODO: is this a bad idea to hit the backend on any route change?  Let's say no!
@@ -20,42 +27,60 @@ export class AuthService implements CanActivate {
       .get<Scrum.User>('api/is_authenticated')
       .pipe(tap(user => (this.user = user)))
   }
-  requestWebsocketAuthToken() {
-    return this.http.get('api/get_websocket_auth_token')
-  }
 
-  getUserInfo() {
-    return this.user
-  }
 
+  private clearUser(){
+    this.user = {email: '', pk: null, displayName: '', is_PM: false}
+  }
   checkCredentials(
     user: {email: string; password: string},
     redirectTo: string | null = null
   ) {
     this.errorMessage = ''
-    this.http.post('/api/login', {...user}).subscribe(
-      success => {
-        this.user.email = user.email
-        const redirectDefault = this.user.is_PM ? '/pm_tools' : '/app/dashboard'
-        this.router.navigate([redirectTo || redirectDefault])
+    this.http.post<LoginReturnType>('/api/login', {...user}).subscribe(
+      successResponse => {
+        this.user = successResponse.user
+        this.websocket.openConnection().subscribe(connOk => {
+          if (connOk !== false){
+            const redirectDefault = this.user.is_PM ? '/pm_tools' : '/app/dashboard'
+            this.router.navigate([redirectTo || redirectDefault])
+          } else {
+            this.errorMessage = 'Unknown Error: Please Try Again'
+            this.clearUser()
+          }
+        })
       },
       error => {
         this.errorMessage = "Invalid Credentials!  We've got a saboteur!"
-        this.user = {email: '', pk: null, displayName: '', is_PM: false}
+        this.clearUser()
       }
     )
     return false
   }
 
   logout() {
+    this.websocket.closeSocket()
     this.http.get('/api/logout').subscribe(
       () => this.router.navigate(['/login']),
       () => this.router.navigate(['/login'])
     )
   }
 
-  queryUser(property: string) {
-    return this?.user[property] ?? null
+  private getUserHelper(propertyFunc: (user: Scrum.User) => valueof<Scrum.User> | Scrum.User ) {
+    const propertyOf /*get it?!*/ = (user: Scrum.User) => of(propertyFunc(user))
+    return this.user === null ?
+      this.isAuthenticated().pipe(
+        switchMap(() => propertyOf(this.user))
+      ) :
+      propertyOf(this.user)
+  }
+
+  queryUser(property: keyof Scrum.User) {
+    return this.getUserHelper(user => user[property]) as Observable<valueof<Scrum.User>>
+  }
+
+  getUserInfo() {
+    return this.getUserHelper(user => user) as Observable<Scrum.User>
   }
 
   async canActivate(route: ActivatedRouteSnapshot) {
