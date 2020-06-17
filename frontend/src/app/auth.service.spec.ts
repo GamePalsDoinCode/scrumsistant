ebc6f4fbf983b5af4ff0f5bc2d5e3386d5b48a93
Danny <danny@cultureiq.com>
2020-06-16 22:26:55 -0400

    some more tests

--- frontend/src/app/auth.service.spec.ts
+++ frontend/src/app/auth.service.spec.ts
@@ -1,19 +1,58 @@
-import {TestBed} from '@angular/core/testing'
-import {HttpClientTestingModule} from '@angular/common/http/testing'
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing'
 import {AuthService} from './auth.service'
 import {RouterTestingModule} from '@angular/router/testing'
+import {TestBed, getTestBed, fakeAsync, tick} from '@angular/core/testing'
+
+const testUser: Scrum.User = {
+  displayName: 'User Display Name',
+  pk: 1,
+  email: 'user@user.com',
+  is_PM: false,
+}
+
+let httpMock: HttpTestingController
 
 describe('AuthService', () => {
   let service: AuthService
+  let injector: TestBed
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule, RouterTestingModule],
     })
     service = TestBed.inject(AuthService)
+    injector = getTestBed()
+    httpMock = injector.get(HttpTestingController)
   })
 
   it('should be created', () => {
     expect(service).toBeTruthy()
+  })
+
+  it('should get properties of authenticated user', async () => {
+    service.user = testUser
+    Object.entries(testUser).forEach(async ([key, val]) => {
+      const queriedVal = await service.queryUser(key).toPromise()
+      expect(queriedVal).toEqual(testUser[key])
+    })
+  })
+  it('should get the entire authenticated user', async () => {
+    service.user = testUser
+    const queriedUser = await service.getUserInfo().toPromise()
+    expect(queriedUser).toBe(testUser)
+  })
+
+  it('should return the authed user when creds are right', () => {
+    ;(service as any)
+      .submitCreds('rightemail@email.com', 'goodPassword')
+      .subscribe((ret: {user: Scrum.User; auth: string}) => {
+        expect(ret.user).toEqual(testUser)
+      })
+    const request = httpMock.expectOne('/api/login')
+    request.flush({user: testUser})
+    expect(request.request.method).toBe('POST')
   })
 })
